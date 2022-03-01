8af8279dfa64e5e066d8994239fd8d6ca015cb81
Sanchez Emir <emir2104@gmail.com>
2022-03-01 05:14:59 -0500

    first commit

---
+++ src/test/java/com/nttdata/yankitransaction/YankiTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.yankitransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YankiTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
